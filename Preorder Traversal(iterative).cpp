15c50fc8459a40af94e3d2a157e0525ae3eb82c2
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2024-07-17 20:30:05 +0530

    Preorder Traversal(iterative)

---
+++ Preorder Traversal(iterative).cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+  vector<int> preorderTraversal(TreeNode* root) {
+    vector<int> preorder;
+    if (!root) return preorder;
+
+    stack<TreeNode*> st;
+    st.push(root);
+    while (!st.empty()) {
+      root = st.top();
+      st.pop();
+      preorder.emplace_back(root -> val);
+
+      if (root -> right) st.push(root -> right);
+      if (root -> left) st.push(root -> left);
+    }
+    return preorder;
+  }
+};
